2bc45a818cea3d83ae9f07a586d157894f70e6f6
MarcoPires <marcofspires@gmail.com>
2016-11-29 21:24:27 +0000

    Installing Middleware

--- login.js
+++ login.js
@@ -1,10 +1,15 @@
 /**
  * npm modules
  */
-var passport      = require('passport');
-var localStrategy = require('passport-local');
-var locallyDB     = require('locallydb');
-var crypto 		  = require('crypto');
+var passport       = require('passport');
+var localStrategy  = require('passport-local');
+var locallyDB      = require('locallydb');
+var crypto         = require('crypto');
+var router         = require('express').Router();
+var expressSession = require('express-session');
+var bodyParser     = require('body-parser');
+var cookieParser   = require('cookie-parser');
+
 
 
 var db    = new locallyDB('./.data');
@@ -58,3 +63,33 @@ passport.deserializeUser(function(cid, done){
 	 */
 	done(null, users.get(cid));
 });
+
+
+/**
+ * This will parse any url with encoded data into a object,
+ * it will be used in the login page
+ */
+router.use(bodyParser.urlencoded({ extended: true }));
+
+/**
+ * Will convert the data sended to the API into JSON
+ */
+router.use(bodyParser.json());
+
+/**
+ * Parse the cookie sended in the requests so we can track the user serialized token
+ */
+router.use(cookieParser());
+
+
+router.use(expressSession({
+	secret            : 'oafajlçfaojiwa892e8ydhmcapw554afag6',
+	resave            : false,
+	saveUninitialized : true
+}));
+
+/**
+ * Bind the passport middleware with the express
+ */
+router.use(passport.initialize());
+router.use(passport.session());

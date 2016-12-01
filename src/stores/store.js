0dbbdd5236327591ad19c3de887097dcd98c6ef2
MarcoPires <marcofspires@gmail.com>
2016-12-01 20:10:05 +0000

    add some doc.

--- src/stores/store.js
+++ src/stores/store.js
@@ -17,42 +17,75 @@ var storeMethods = {
 
 	init: function(){return this;}, 
 
-	set: function(arr){
+	/**
+	 * Save a list of uniques items, 
+	 * each item must contains a cid property
+	 * @param {array} list
+	 */
+	set: function(list){
 		var currIds 	= this._data.map(function(item){ return item.cid }),
-			uniqueItems = arr.filter(function(item){ return currIds.indexOf(item.cid) === -1; });
+			uniqueItems = list.filter(function(item){ return currIds.indexOf(item.cid) === -1; });
 
 		uniqueItems.forEach(this.add.bind(this));
 		return this;
 	},
 
+	/**
+	 * Save a single item
+	 * @param {object} item 
+	 */
 	add: function(item){
 		this._data.push(item);
 		return this;
 	},
 
+	/**
+	 * Returns all saved items
+	 * @return {array} 
+	 */
 	getAll: function(){
 		return this._data;
 	},
 
+	/**
+	 * Returns a item that matchs id
+	 * @param  {number} id
+	 * @return {object}    
+	 */
 	getById: function(id){
 		return this._data.filter(function(item){ return item.cid === id })[0];
 	},
 
+	/**
+	 * Registers a function interested in state changes
+	 * @param {function} callback 
+	 */
 	addChangeListener: function(callback){
 		this.on(CHANGE_EVENT, callback);
 		return this;
 	},
-
+	/**
+	 * Unregisters a function that is not any longer interested in state changes
+	 * @param  {function} callback 
+	 */
 	removeChangeListener: function(callback){
 		this.removeListener(CHANGE_EVENT, callback);
 		return this;
 	},
 
+	/**
+	 * Informs all the interested in the state change
+	 */
 	emitChange: function() {
 		this.emit(CHANGE_EVENT);
 		return this;
 	},
 
+	/**
+	 * Binds actions to state changes
+	 * @param  {string} actionType     
+	 * @param  {function} actionCallback 
+	 */
 	bind: function(actionType, actionCallback) {
 		if(!this.actions[actionType]) this.actions[actionType] = [];
 		this.actions[actionType].push(actionCallback);
